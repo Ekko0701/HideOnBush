333edb707af192d83edeb532ed46a735886686d6
Ekko0701 <djekko0701@gmail.com>
2026-06-26 14:09:10 +0900

    build: update cask for notarized release

--- Casks/hideonbush.rb
+++ Casks/hideonbush.rb
@@ -1,6 +1,6 @@
 cask "hideonbush" do
   version "0.1.0"
-  sha256 "cc534cdcf6e80284217f950a23652abd0b4f255deb3d13cf6ab141316e76f668"
+  sha256 "8cc37740735b3b67e0f7a839d8c8244dc8a0ac9e05f418d0bd1fa2a7ebb65b88"
 
   url "https://github.com/Ekko0701/HideOnBush/releases/download/v#{version}/HideOnBush-v#{version}-macos-arm64.zip",
       verified: "github.com/Ekko0701/HideOnBush/"
